39a459d449ccff052fab0f92ab6c9bf9775f5b6e
Lambert Mateo <mateo.lambert@etu.umontpellier.fr>
2023-11-07 17:25:54 +0100

    ajout de la classe CommandeMajuscules

---
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscules.java
@@ -0,0 +1,21 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeMajuscules extends CommandeDocument{
+    public CommandeMajuscules(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : majuscules;start;end");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        this.document.majuscules(start, end);
+        super.executer();
+    }
+}
