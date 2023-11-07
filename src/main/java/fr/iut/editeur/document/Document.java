aaef8afb3f6ac08ab82294c6575e3a3b243a1551
Lambert Mateo <mateo.lambert@etu.umontpellier.fr>
2023-11-07 17:24:23 +0100

    majuscules dans Document

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -30,4 +30,8 @@ public class Document {
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
+    public void majuscules(int start, int end) {
+        String chaineMaj = texte.substring(start, end).toUpperCase();
+        remplacer(start, end, chaineMaj);
+    }
 }
